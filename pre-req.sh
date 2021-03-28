23fda49264e325200ab0f0b8336456afc7aacc98
Alex Junio <contato@alejunio.cloud>
2021-03-28 12:46:22 -0300

    Ajustes PHP

--- pre-req.sh
+++ pre-req.sh
@@ -32,9 +32,6 @@ snap install core
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
 
-# Instalacao Composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-
+# Repositorio Ondrej/PHP
+add-apt-repository --yes --update ppa:ondrej/php
+apt-get update
